7167e443fabf7dbacdefa9ac71b1c3a662d074fa
JianxinLi <justcoxier@gmail.com>
2018-01-23 15:18:23 +0800

    init commit
    Change-Id: Ic49f6b7ab8ef0b39553a986b4fff39c2c9c32c22

---
+++ account_info.py
@@ -0,0 +1,18 @@
+# coding : utf-8
+
+import os
+import json
+
+
+class Account:
+    email = ''
+    password = ''
+
+    def __init__(self) -> None:
+        current_dir = os.path.dirname(__file__)
+        json_file = os.path.join(current_dir, 'config/account.json')
+        json_data = json.load(open(json_file))
+        self.email = json_data.get('email')
+        self.password = json_data.get('password')
+
+
---
+++ stack_login.py
@@ -0,0 +1,57 @@
+# coding : utf-8
+
+from account_info import Account
+import requests
+import re
+import datetime
+
+
+class StackOverflow:
+    base_url = 'https://stackoverflow.com/users/login'
+    fkey = ''
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+    }
+
+    params = {
+        "ssrc": "head",
+        "returnurl": "https://stackoverflow.com/",
+    }
+
+    def login(self):
+        print('Start login ...')
+        account = Account()
+        print('Email : {}\nPassword : {}'.format(account.email, account.password))
+        payload = self.get_payload(account)
+        response = requests.post(self.base_url, data=payload, headers=self.headers, params=self.params,
+                                 allow_redirects=False)
+        if response.status_code == 302:
+            print("\033[92mLogged in:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+        else:
+            print("\033[91mPlease check your email and password."
+                  "If nothing wrong, please report this issue to https://github.com/CoXier/stackoverflow-login/issues")
+
+    def get_fkey(self):
+        print('Retrieving fkey information...')
+        response = requests.get(self.base_url, params=self.params, headers=self.headers)
+        return re.search(r'"fkey":"([^"]+)"', response.text).group(1)
+
+    def get_payload(self, account):
+        self.fkey = self.get_fkey()
+        return {
+            'openid_identifier': '',
+            'password': account.password,
+            'fkey': self.fkey,
+            'email': account.email,
+            'oauth_server': '',
+            'oauth_version': '',
+            'openid_username': '',
+            'ssrc': 'head',
+        }
+
+
+if __name__ == '__main__':
+    stack_overflow = StackOverflow()
+    stack_overflow.login()

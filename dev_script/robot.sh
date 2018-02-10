172cf0f56977d750b261ce748c25a67fe4c0c689
JianxinLi <justcoxier@gmail.com>
2018-02-10 12:00:26 +0800

    Feature: check venv environment exists
    Change-Id: I7e519212228c851108e40f7b4c9cf3f7e8bdc0e7

--- dev_script/robot.sh
+++ dev_script/robot.sh
@@ -2,5 +2,12 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 cd ..
-source venv/bin/activate
+venv_file="venv/bin/activate"
+if (! [ -e "$venv_file" ])
+then
+    RED='\033[0;31m'
+    NC='\033[0m'
+    echo -e "${RED}Python venv not configured${NC}"
+fi
+source "$venv_file"
 python stack_login.py >> log.txt
